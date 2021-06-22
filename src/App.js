c207adb52e35578f3a4d838cc84f6fc4fe47e0b7
Selma456 <74097172+sm456456@users.noreply.github.com>
2021-06-22 18:54:37 +0200

    add logo and tilt function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageInputForm from './components/ImageInputForm/ImageInputForm';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* 
       <Logo />
       <ImageInputForm />
+      {/* 
       <FaceDetection /> */}
     </div>
   );
---
+++ src/components/ImageInputForm/ImageInputForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ImageInputForm = () => {
+  return (
+    <div>
+      <p>
+        This computer brain can detect faces on the photos. Paste URL of your photo to give it a try.
+      </p>
+    </div>
+  )
+}
+
+export default ImageInputForm
