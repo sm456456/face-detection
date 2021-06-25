f9b835e0cef3744d733c8b5830d974c2acc42cc9
Selma456 <74097172+sm456456@users.noreply.github.com>
2021-06-25 19:04:39 +0200

    change from fucntion to a class

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import './App.css';
-import Particles from 'react-particles-js';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageInputForm from './components/ImageInputForm/ImageInputForm';
+import React, { Component } from "react"
+import "./App.css"
+import Particles from "react-particles-js"
+import Navigation from "./components/Navigation/Navigation"
+import Logo from "./components/Logo/Logo"
+import ImageInputForm from "./components/ImageInputForm/ImageInputForm"
+import FaceDetection from "./components/FaceDetection/FaceDetection"
 
-function App() {
-  return (
-    <div className="App">
-      <Particles />
-      <Navigation />
-      <Logo />
-      <ImageInputForm />
-      {/* 
-      <FaceDetection /> */}
-    </div>
-  );
+const initialState = {
+  input: "",
+  imageUrl: "",
+  box: {},
+  route: "signin",
+  isSignedIn: false,
+  user: {
+    id: "",
+    name: "",
+    email: "",
+    entries: 0,
+    joined: ""
+  }
+}
+class App extends Component {
+  constructor() {
+    super()
+    this.state = initialState
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles params={{}} />
+        <Navigation />
+        <Logo />
+        <ImageInputForm />
+        <FaceDetection />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
